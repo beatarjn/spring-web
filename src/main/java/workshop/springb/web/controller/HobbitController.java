11eb8c2ef26e66abebd357e1ef17ca98c40c3201
Your Name <you@example.com>
2020-07-14 17:12:46 +0200

    TODO

--- src/main/java/workshop/springb/web/controller/HobbitController.java
+++ src/main/java/workshop/springb/web/controller/HobbitController.java
@@ -1,9 +1,9 @@
 package workshop.springb.web.controller;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 import workshop.springb.web.model.Hobbit;
-
-import java.util.Map;
 
 @RestController
 public class HobbitController {
@@ -13,23 +13,40 @@ public class HobbitController {
         return new Hobbit(name.toUpperCase(), lastName.toUpperCase());
     }
 
-    @GetMapping("/hobbits-arguments")
-    public Hobbit requestParamExample(@RequestParam String name, @RequestParam String lastName) {
-        return new Hobbit(name.toUpperCase(), lastName.toUpperCase());
-
-    }
-
-    @PostMapping("/hobbits-json-object")
-    public Hobbit requestBodyExample(@RequestBody Hobbit hobbit) {
-        hobbit.setName(hobbit.getName().toUpperCase());
-        hobbit.setLastName(hobbit.getLastName().toUpperCase());
-        return hobbit;
-
-    }
-
-    @GetMapping("/hobbits-matrix/{hobbit-data}")
-    public Hobbit matrixVariableExample(@MatrixVariable Map<String, String> matrix) {
-        return new Hobbit(matrix.get("name").toUpperCase(), matrix.get("lastName").toUpperCase());
-    }
-
 }
+    /*
+    ____________________________________________________________________________________________________________________
+    TODO W tym module przećwiczymy 4 sposoby na przyjmowanie danych z żądania HTTP:
+     @PathVariable, @RequestParam, @RequestBody, @MatrixVariable
+
+    Dla @PathVariable mamy już implementację.
+    Utwórz endpoint'y i testy dla pozostałych przypadków.
+
+    DoD:
+
+    Wszystkie testy w HobbitControllerTest na zielono :).
+
+    Potrzebujemy:
+
+    4 endpointy - każdy z nich przyjmuje dane, tworzy na ich podstawie Hobbit'a.
+    Przyjmowane dane (w różnej formie) to name i lastName Hobbita.
+
+    Zwracamy hobbita z name i lastName pisanymi wielką literą:
+
+    Bilbo Baggins - > BILBO BAGGINS
+
+    Uwaga - implementujemy najprostsze wariant - bez zabezpieczenia przed NullPointerException itp.
+
+
+    Zwróć uwagę na klasę konfiguracyjną workshop.springb.web.config.WebConfig,
+    ta dodatkowa konfiguracja potrzebna jest dla użycia @MatrixVariable
+    ____________________________________________________________________________________________________________________
+                                                         \
+                                                          \
+                                                            /  \~~~/  \
+                                                           (    ..     )----,
+                                                            \__     __/      \
+                                                              )|  /)         |\
+                                                               | /\  /___\   / ^
+                                                                "-|__|   |__|
+    */
